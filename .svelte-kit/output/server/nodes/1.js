0efd433f895e1eb7f95c1e78c3ddcf043681a462
Jem <jem@Jems-MacBook-Pro.local>
2025-08-17 19:20:56 +0800

    Temporarily disable PWA to fix navigation issues

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BqkqgJ59.js","_app/immutable/chunks/Cm9PB-7n.js","_app/immutable/chunks/BZBr9UGP.js","_app/immutable/chunks/0VPK0fgq.js","_app/immutable/chunks/DlFSuV4-.js"];
+export const imports = ["_app/immutable/nodes/1.CpYx7pnS.js","_app/immutable/chunks/Cm9PB-7n.js","_app/immutable/chunks/BZBr9UGP.js","_app/immutable/chunks/0VPK0fgq.js","_app/immutable/chunks/EePE5J4F.js"];
 export const stylesheets = [];
 export const fonts = [];
